16b2e6f88175285b8d3c553d108a83ae89eae26c
Ritikag33 <ritikag5533@gmail.com>
2024-03-05 13:21:11 +0530

    logging.py and exception.py

---
+++ src/logger.py
@@ -0,0 +1,23 @@
+# Log all the execution in this file to handle error, custom exception 
+import logging
+import os 
+from datetime import datetime 
+
+# Text file created with this naming convention
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+# Every file name should start with logs
+logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
+
+
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+logging.basicConfig(
+    filename = LOG_FILE_PATH,
+    format   = "[%(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level    = logging.INFO,
+)
+
+
+# if __name__ == "__main__":
+#     logging.info("Logging has started")
